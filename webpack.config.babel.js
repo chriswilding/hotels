bbbefaab9b33a8fadc9178fa9bb3945bdf7b239f
Chris Wilding <hello@chriswilding.co.uk>
2018-01-28 14:25:32 +0000

    Add prettier with husky and lint-staged

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,30 +1,29 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
 
 export default {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   module: {
     loaders: [
       {
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        test: /\.(js|jsx)$/,
-      },
-    ],
+        loader: "babel-loader",
+        test: /\.(js|jsx)$/
+      }
+    ]
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      inject: 'body',
-      template: './public/index.html',
-    }),
+      filename: "index.html",
+      inject: "body",
+      template: "./public/index.html"
+    })
   ],
   resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    extensions: [".js", ".jsx"]
+  }
 };
-
