8a9b7b6659a96a4d42febb34446bbb247ac281d2
Chris Wilding <hello@chriswilding.co.uk>
2018-01-28 16:10:08 +0000

    Add Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { render } from "react-dom";
 
 import App from "./components/App";
+import reducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
