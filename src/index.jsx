8b8ba31d9ed4dae618e1ba7625e47175eeb52a8d
Chris Wilding <hello@chriswilding.co.uk>
2018-01-28 22:24:21 +0000

    Remove Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,6 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
 import { render } from "react-dom";
 
 import HotelSearchResultsPageContainer from "./containers/HotelSearchResultsPageContainer";
-import reducer from "./reducers";
 
-const store = createStore(reducer);
-
-render(
-  <Provider store={store}>
-    <HotelSearchResultsPageContainer />
-  </Provider>,
-  document.getElementById("root")
-);
+render(<HotelSearchResultsPageContainer />, document.getElementById("root"));
